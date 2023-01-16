769311b738b9316f1c326eba0d0e618ece72fbe9
ali-kbader <t-alibader@microsoft.com>
2023-01-16 17:16:09 +0200

    added stemming option and if statement

--- helpers/search_helpers.py
+++ helpers/search_helpers.py
@@ -7,14 +7,29 @@ number_of_docs = 6348910
 doc_avg_mean = 320
 
 
-def binary_ranking(index, postings_path, query):
+def binary_ranking(index, postings_path, query, stemming=False):
     index_terms = index.df.keys()
     query_tokens = tokenize_removeStopWord(query)
     unique_tokens = np.unique(query_tokens)
-    relevant_query_tokens = filter(lambda term: term in index_terms, unique_tokens)
+    relevant_query_tokens = list(filter(lambda term: term in index_terms, unique_tokens))
+    stemmed_tokens = stem_tokens(query_tokens)
+    combined_tokens = []
+
+    for token in stemmed_tokens:
+        combined_tokens.append(token)
+
+    for token in relevant_query_tokens:
+        combined_tokens.append(token)
+
+    combined_tokens = np.unique(combined_tokens)
+
+    if stemming:
+        relevant_query_tokens = combined_tokens
+
     doc_id_freq_count = {}
 
-    for term in unique_tokens:
+    for term in relevant_query_tokens:
+        # if term in index_terms:
         posting_list = index.read_posting_list(term, postings_path)
 
         for doc_id, doc_freq in posting_list:
@@ -37,14 +52,15 @@ def BM25(doc_id_len, query, index, path, k1=1.2, b=0.75):
 
     scores = {}
     for term in query_vector:
-        posting_list = index.read_posting_list(term, path)
-        for doc_id, tf in posting_list:
-            idf = math.log((number_of_docs - index.df[term] + 0.5) / (index.df[term] + 0.5))
-            doc_length = int(doc_id_len.get(str(doc_id), str(doc_avg_mean)))
-            score = (idf * tf * (k1 + 1) / (tf + k1 * (1 - b + b * (doc_length / doc_avg_mean))))
-            if doc_id in scores:
-                scores[doc_id] += score
-            else:
-                scores[doc_id] = score
+        if term in index.df:
+            posting_list = index.read_posting_list(term, path)
+            for doc_id, tf in posting_list:
+                idf = math.log((number_of_docs - index.df[term] + 0.5) / (index.df[term] + 0.5))
+                doc_length = int(doc_id_len.get(str(doc_id), str(doc_avg_mean)))
+                score = (idf * tf * (k1 + 1) / (tf + k1 * (1 - b + b * (doc_length / doc_avg_mean))))
+                if doc_id in scores:
+                    scores[doc_id] += score
+                else:
+                    scores[doc_id] = score
 
     return scores
